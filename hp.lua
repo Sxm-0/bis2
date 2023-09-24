c7479b94e9d991f3b6387cde76dfb73f75a35acd
Sxm <95942743+Sxm-0@users.noreply.github.com>
2023-09-24 18:38:04 +0100

    Update hp.lua

--- hp.lua
+++ hp.lua
@@ -1,23 +1,25 @@
-spawn(function()
-	while energy == true and task.wait() do
-		if game:GetService("Players").GoldenGuyWasTaken.PlayerGui.EnergyBar.EnergyBar.EnergyBar:FindFirstChild("Template") then
-			game:GetService("Players").GoldenGuyWasTaken.PlayerGui.EnergyBar.EnergyBar.EnergyBar:FindFirstChild("Template"):Destroy()
-		else
-			return ""
-		end
-  
-		if game:GetService("Players").GoldenGuyWasTaken.PlayerGui.EnergyBar.EnergyBar.EnergyBar.ImageLabel.Frame.Size ~= UDim2.new(1, 0, 1, 0) then
-  		local args = {
-  			[1] = "Apple"
-  		}
-  
-  		game:GetService("ReplicatedStorage").Events.GiveTool:FireServer(unpack(args))
-  		local args = {
-  			[1] = 15,
-  			[2] = "Apple"
-  		}
-  
-  		game:GetService("ReplicatedStorage").Events.Energy:FireServer(unpack(args))
+_G.Energy = false
+
+local function InfHealth()
+    while _G.Energy do
+        local template = game.Players.GoldenGuyWasTaken.PlayerGui.EnergyBar.EnergyBar.EnergyBar:FindFirstChild("Template")
+        
+        if template then
+            template:Destroy()
+        end
+        if game:GetService("Players").GoldenGuyWasTaken.PlayerGui.EnergyBar.EnergyBar.EnergyBar.ImageLabel.Frame.Size ~= UDim2.new(1, 0, 1, 0) then
+            local args = {
+                [1] = "Apple"
+            }
+
+            game:GetService("ReplicatedStorage").Events.GiveTool:FireServer(unpack(args))
+            local args = {
+                [1] = 15,
+                [2] = "Apple"
+            }
+
+            game:GetService("ReplicatedStorage").Events.Energy:FireServer(unpack(args))
+        end
+        task.wait()
     end
 end
-end)
